466f9cedee70a1ff2e5532fbe3ff7759a16add4e
allvideodownload <hn288804@gmail.com>
2024-10-11 19:37:09 +0530

    Create CloudEstimatorApplication.java

---
+++ backend/src/main/java/com/estimator/CloudEstimatorApplication.java
@@ -0,0 +1,11 @@
+package com.estimator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudEstimatorApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(CloudEstimatorApplication.class, args);
+    }
+}
