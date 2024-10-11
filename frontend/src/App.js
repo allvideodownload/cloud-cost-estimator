9192aa679f003afd3c0bf69db7d4d37f6bdb222c
allvideodownload <hn288804@gmail.com>
2024-10-11 19:42:02 +0530

    Create App.js

---
+++ frontend/src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import CostEstimatorForm from "./components/CostEstimatorForm";
+
+const App = () => {
+    return (
+        <div>
+            <h1>Cloud Cost Estimator</h1>
+            <CostEstimatorForm />
+        </div>
+    );
+};
+
+export default App;
